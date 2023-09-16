2af22e7a116c02d9f70d9adbf5a9b29a07c1f9b5
JoaquinFlores1705 <paul_1705@outlook.com>
2023-09-16 11:14:36 -0500

    Footer en mobile

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class=" py-2 border-t-2 border-third bg-panel dark:bg-panel-dark dark:text-white hidden lg:block">
-    <div class="container flex justify-between items-center">
+<footer class=" py-2 border-t-2 border-third bg-panel dark:bg-panel-dark dark:text-white">
+    <div class="container flex flex-col items-center lg:flex-row lg:justify-between lg:items-center">
         <p class="text-sm">Version Beta 1.0 (2023-2024)</p>
         <p class="text-sm">Todos los derechos reservados©</p>
     </div>
