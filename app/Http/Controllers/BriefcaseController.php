b38c81601308bcbc23bdd3ce2b1734959837d267
JoaquinFlores1705 <paul_1705@outlook.com>
2023-09-16 10:59:13 -0500

    Datos en variables arrays Demo 1.0

--- app/Http/Controllers/BriefcaseController.php
+++ app/Http/Controllers/BriefcaseController.php
@@ -5,7 +5,143 @@ namespace App\Http\Controllers;
 class BriefcaseController extends Controller {
 
     public function show(){
+        $experiences = [];
+        $educations = [];
+        $references = [];
+        $skills = [];
 
-        return view('welcome');
+        array_push($experiences, 
+            [
+                "post" => "Desarrollador FullStack .Net y Angular",
+                "company" => "Pervasive Mind/Edenred",
+                "country" => "Chile",
+                "dates" => "Nov 2021 - Jun 2023",
+                "description" => "Era responsable de hacer mejoras y resolver incidentes en 
+                diferentes microservicios basados en .Net Core y agregar 
+                funcionalidad a SPA en Angular, documentar cambios, trabajar a 
+                la par con el área de QA y participar en reuniones para mejoras 
+                en diferentes plataformas y utilitarios de la empresa Edenred",
+                "technologies" => "resources/img/c-sharp-c.svg,resources/img/angular.png",
+                "extra-class" => ""
+            ],
+            [
+                "post" => "Desarrollador .Net C#",
+                "company" => "TTA/Produbanco",
+                "country" => "Ecuador",
+                "dates" => "Nov 2020 - Nov 2021",
+                "description" => "Responsable de la migración de la ventanilla de Produbanco 
+                basada en vb 6 a Formularios y paginas ASP de .Net c#",
+                "technologies" => "resources/img/c-sharp-c.svg",
+                "extra-class" => ""
+            ],
+            [
+                "post" => "Desarrollador FullStack .Net y Angular",
+                "company" => "Easysoft/Produbanco",
+                "country" => "Ecuador",
+                "dates" => "Jul 2018 - Nov 2020",
+                "description" => "Responsable de agregar mejoras y corregir diferentes 
+                funcionalidades del aplicativo del CashManagement en 
+                Produbanco - Grupo Promerica basado en Angular y 
+                microservicios en C# y en menor medida Python",
+                "technologies" => "resources/img/c-sharp-c.svg,resources/img/angular.png,resources/img/python.png",
+                "extra-class" => "border-b-0 pb-0"
+            ],
+        );
+
+        array_push($educations, 
+            [
+                "post" => "Ingeniería en Sistemas",
+                "company" => "Universidad Laica Eloy Alfador de Manabí",
+                "country" => "Ecuador",
+                "dates" => "Abr 2010 - Sep 2017",
+                "description" => "Titulado como Ingeniero en sistemas de la República del Ecuador 
+                promoción 2017",
+                "extra-class" => ""
+            ]
+        );
+
+        array_push($references, 
+            [
+                "image" => "resources/img/IrisUzcategui.jpg",
+                "name" => "Iris Uzcategui",
+                "post" => "Tech Lead Edenred",
+                "phone" => "+56930247928",
+                "extra-class" => ""
+            ],
+            [
+                "image" => "resources/img/CarlosNovoa.jpg",
+                "name" => "Carlos Novoa",
+                "post" => "Tech Lead FrontEnd Produbanco",
+                "phone" => "+593988880072",
+                "extra-class" => ""
+            ],
+            [
+                "image" => "resources/img/HugoMazzini.png",
+                "name" => "Hugo Mazzini",
+                "post" => "Jefe de IT CashManagement",
+                "phone" => "+593996715801",
+                "extra-class" => ""
+            ],
+        );
+
+        array_push($skills, 
+            [
+                "technology" => "C#",
+                "ability" => "5/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "Angular",
+                "ability" => "5/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "HTML",
+                "ability" => "5/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "CSS",
+                "ability" => "5/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "Javascript",
+                "ability" => "4/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "React",
+                "ability" => "4/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "SQL",
+                "ability" => "4/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "Node.JS",
+                "ability" => "3/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "Python",
+                "ability" => "3/5",
+                "extra-class" => ""
+            ],
+            [
+                "technology" => "Flutter",
+                "ability" => "3/5",
+                "extra-class" => ""
+            ],
+        );
+
+        return view('welcome')->with([
+            "experiences" => $experiences,
+            "educations" => $educations,
+            "references" => $references,
+            "skills" => $skills
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
                         </svg> 
                         <span>paul_1705@outlook.com</span>
                     </a>
-                    <a href="tel:593998001121" class="flex lg:justify-center lg:basis-full">
+                    <a href="tel:+593998001121" class="flex lg:justify-center lg:basis-full">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-bcolor font-bold">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                         </svg>
@@ -54,135 +54,71 @@
                 <span class="span-title">Habilidades</span>  
               </p>
               <ul class="flex justify-between flex-wrap">
-                  <li class="basis-1/2">C#</li>
-                  <li>⭐⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">Angular</li>
-                  <li>⭐⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">HTML</li>
-                  <li>⭐⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">CSS</li>
-                  <li>⭐⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">Javascript</li>
-                  <li>⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">React</li>
-                  <li>⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">SQL</li>
-                  <li>⭐⭐⭐⭐</li>
-                  <li class="basis-1/2">Node.JS</li>
-                  <li>⭐⭐⭐</li>
-                  <li class="basis-1/2">Python</li>
-                  <li>⭐⭐⭐</li>
-                  <li class="basis-1/2">Flutter</li>
-                  <li>⭐⭐⭐</li>
+                @foreach ($skills as $skill)
+                    <?php 
+                        list($fullStar,$totalStar) = explode("/",$skill["ability"]);
+                        $stars = "";
+                        for($i = 1; $i <= $fullStar; $i++){
+                            $stars .= "⭐";
+                        }
+                    ?>
+                    <li class="basis-1/2">{{ $skill["technology"] }}</li>
+                    <li class="text-yellow-400 flex items-center">{{$stars}}@for ($i = 1; $i <= $totalStar - $fullStar; $i++)<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" /></svg>
+                        @endfor</li>
+                @endforeach
               </ul>
         </section>
         <section id="experience" class="separation-bcolor lg:basis-full lg:order-2">
             <span class="span-title">Experiencia laboral</span>
-            <div class="text-sm">
-                <h1 class="text-lg">Desarrollador FullStack .Net y Angular</h1>
-                <h2 class="text-base">Pervasive Mind/Edenred</h2>
-                <h3 class="flex justify-between text-gray-700 dark:text-gray-300">
-                    <p>Chile</p>
-                    <p>Nov 2021 - Jun 2023</p>
-                </h3>
-                <p>Era responsable de hacer mejoras y resolver incidentes en 
-                    diferentes microservicios basados en .Net Core y agregar 
-                    funcionalidad a SPA en Angular, documentar cambios, trabajar a 
-                    la par con el área de QA y participar en reuniones para mejoras 
-                    en diferentes plataformas y utilitarios de la empresa Edenred
-                </p>
-                <div class="separation-experience">
-                    <p>Tecnologías: </p>
-                    <div class="flex">
-                        <img src="{{ Vite::asset('resources/img/c-sharp-c.svg') }} " class="w-6 h-6"/>
-                        <img src=" {{ Vite::asset('resources/img/angular.png') }}" class="w-6 h-6"/>
+            @foreach ($experiences as $experience)
+                <div class="text-sm">
+                    <h1 class="text-lg">{{ $experience["post"] }}</h1>
+                    <h2 class="text-base">{{ $experience["company"] }}</h2>
+                    <h3 class="flex justify-between text-gray-700 dark:text-gray-300">
+                        <p>{{ $experience["country"] }}</p>
+                        <p>{{ $experience["dates"] }}</p>
+                    </h3>
+                    <p>{{ $experience["description"] }}</p>
+                    <div class="separation-experience {{ $experience["extra-class"] }}">
+                        <p>Tecnologías: </p>
+                        <div class="flex">
+                            @foreach (explode(",",$experience["technologies"]) as $technology)
+                                <img src="{{ Vite::asset($technology) }} " class="w-6 h-6"/>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>     
-            <div class="text-sm mt-2">
-                <h1 class="text-lg">Desarrollador .Net C#</h1>
-                <h2 class="text-base">TTA/Produbanco</h2>
-                <h3 class="flex justify-between text-gray-700 dark:text-gray-300">
-                    <p>Ecuador</p>
-                    <p>Nov 2020 - Nov 2021</p>
-                </h3>
-                <p>Responsable de la migración de la ventanilla de Produbanco 
-                    basada en vb 6 a Formularios y paginas ASP de .net c#
-                </p>
-                <div class="separation-experience">
-                    <p>Tecnologías: </p>
-                    <div class="flex">
-                        <img src="{{ Vite::asset('resources/img/c-sharp-c.svg') }} " class="w-6 h-6"/>
-                    </div>
-                </div>
-            </div>
-            <div class="text-sm mt-2">
-                <h1 class="text-lg">Desarrollador FullStack .Net y Angular</h1>
-                <h2 class="text-base">Easysoft/Produbanco</h2>
-                <h3 class="flex justify-between text-gray-700 dark:text-gray-300">
-                    <p>Ecuador</p>
-                    <p>Jul 2018 - Nov 2020</p>
-                </h3>
-                <p>Responsable de agregar mejoras y corregir diferentes 
-                    funcionalidades del aplicativo del CashManagement en 
-                    Produbanco - Grupo Promerica basado en Angular y 
-                    microservicios en C# y en menor medida Python
-                </p>
-                <div class="separation-experience border-b-0 pb-0">
-                    <p>Tecnologías: </p>
-                    <div class="flex">
-                        <img src="{{ Vite::asset('resources/img/c-sharp-c.svg') }} " class="w-6 h-6"/>
-                        <img src=" {{ Vite::asset('resources/img/angular.png') }}" class="w-6 h-6"/>
-                        <img src="{{ Vite::asset('resources/img/python.png') }}" class="w-6 h-6"/>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </section>
         <section class="separation-bcolor lg:basis-full lg:order-2 lg:border-b-0" id="education">
             <span class="span-title">Educación</span>
-            <div class="text-sm">
-                <h1 class="text-lg">Ingeniería en Sistemas</h1>
-                <h2 class="text-base">Universidad Laica Eloy Alfador de Manabí</h2>
-                <h3 class="flex justify-between text-gray-700 dark:text-gray-300">
-                    <p>Ecuador</p>
-                    <p>Abr 2010 - Sep 2017</p>
-                </h3>
-                <p>Titulado como Ingeniero en sistemas de la República del Ecuador 
-                    promoción 2017
-                </p>
-            </div>
+            @foreach ($educations as $education)
+                <div class="text-sm">
+                    <h1 class="text-lg">{{ $experience["post"] }}</h1>
+                    <h2 class="text-base">{{ $experience["company"] }}</h2>
+                    <h3 class="flex justify-between text-gray-700 dark:text-gray-300">
+                        <p>{{ $experience["country"] }}</p>
+                        <p>{{ $experience["dates"] }}</p>
+                    </h3>
+                    <p>{{ $experience["description"] }}</p>
+                </div>
+            @endforeach
         </section>
         <section class="separation-bcolor border-b-0 lg:basis-2/4 lg:order-1 lg:separation-bcolor lg:pl-2" id="references">
             <span class="span-title">Referencias</span>
             <div class="mt-2 lg:flex lg:h-full lg:content-center">
                 <ul class="flex justify-between gap-y-1 flex-wrap text-sm">
-                    <li class="flex gap-2 basis-1/2">
-                        <img src="{{ Vite::asset('resources/img/IrisUzcategui.jpg') }} " class="w-6 h-6 rounded-full border-1 border-bcolor"/>
-                        <p>Iris Uzcategui(Tech Lead Edenred)</p>
-                    </li>
-                    <li>
-                        <a href="tel:5630247928">
-                            +5630247928
-                        </a>
-                    </li>
-                    <li class="flex gap-2 basis-1/2">
-                        <img src="{{ Vite::asset('resources/img/CarlosNovoa.jpg') }} " class="w-6 h-6 rounded-full border-1 border-bcolor"/>
-                        <p>Carlos Novoa(Tech Lead FrontEnd Produbanco)</p>
-                    </li>
-                    <li>
-                        <a href="tel:593988880072">
-                            +593988880072
-                        </a>
-                    </li>
-                    <li class="flex gap-2 basis-1/2">
-                        <img src="{{ Vite::asset('resources/img/HugoMazzini.png') }} " class="w-6 h-6 rounded-full border-1 border-bcolor"/>
-                        <p>Hugo Mazzini(Jefe de IT CashManagement)</p>
-                    </li>
-                    <li>
-                        <a href="tel:593996715801">
-                            +593996715801
-                        </a>
-                    </li>
+                    @foreach ($references as $reference)
+                        <li class="flex gap-2 basis-1/2">
+                            <img src="{{ Vite::asset( $reference["image"] ) }} " class="w-6 h-6 rounded-full border-1 border-bcolor"/>
+                            <p>{{ $reference["name"] }}({{ $reference["post"] }})</p>
+                        </li>
+                        <li>
+                            <a href="tel:{{ $reference["phone"] }}">
+                                {{ $reference["phone"] }}
+                            </a>
+                        </li>
+                    @endforeach
                 </ul>
             </div>
         </section>
