c08081f9417c3e3c6f1d07295d14e2ae0814caed
JoaquinFlores1705 <paul_1705@outlook.com>
2023-09-16 03:12:05 -0500

    Footer fecha

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class=" py-2 border-t-2 border-third bg-panel dark:bg-panel-dark dark:text-white hidden lg:block">
     <div class="container flex justify-between items-center">
-        <p class="text-sm">Version Beta 1.0</p>
-        <p class="text-sm">Todos los derechos reservados ©</p>
+        <p class="text-sm">Version Beta 1.0 (2023-2024)</p>
+        <p class="text-sm">Todos los derechos reservados©</p>
     </div>
 </footer>
